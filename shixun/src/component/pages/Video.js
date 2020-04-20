fee2a27835d94eb77bbdf4084104a1f1ac408f95
daifang <784433957@qq.com>
2020-04-20 22:14:41 +0800

    vedio

--- shixun/src/component/pages/Video.js
+++ shixun/src/component/pages/Video.js
@@ -3,7 +3,23 @@ import Body from '../Body';
 import {Link,HashRouter as Router,} from 'react-router-dom';
 import Box from '../Box';
 import Header from '../Header';
+import {Player} from 'video-react';
+import './../../../node_modules/video-react/dist/video-react.css';
 export default class Video extends Component {
+    constructor(){
+        super()
+        this.state = {
+            data:[]
+        }
+    }
+    componentDidMount(){
+        fetch('https://daitianfang.1459.top/api/v1/video').then(data=>data.json()).then(res=>{
+            this.setState({
+                data:res.data
+            })
+        })
+        //弹幕加载
+    }
     render() {
         return (
             <div>
@@ -12,7 +28,17 @@ export default class Video extends Component {
                 </div>
                 <div style={{height:'120px'}}>
                 </div>
-                <Body url='https://daitianfang.1459.top/api/v1/chapter?type=game'/>
+                <div>
+                    {
+                        this.state.data.map(val=>{
+                            <div className='video'>
+                                <Player>
+                                    <source src={val.vedio_src}/>
+                                </Player> 
+                            </div>
+                        })
+                    }
+                </div>
                 <div id='footer'>
                         <Link to='/apphome'>
                         <Box src='/img/首页.png' title='首页' />
